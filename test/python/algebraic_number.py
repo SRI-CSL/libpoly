bbe6d5cbb4b8acd7f4a32ccceb1c813aa40efc2a
Dejan Jovanovic <dejan.jovanovic@gmail.com>
2014-03-24 01:02:46 -0700

    last of printouts in check

--- test/python/algebraic_number.py
+++ test/python/algebraic_number.py
@@ -5,48 +5,71 @@ import polypy_test
 
 from polypy import x
 
+def check_comparison(a1, a2, cmp, result, expected):
+    if (result != expected):
+        print "a1 =", a1
+        print "a2 =", a2
+        print "cmp =", cmp
+        print "result =", result
+        print "expected =", expected
+        polypy_test.check(False)
+    else:
+        polypy_test.check(True)
+        
 polypy_test.init()
-polypy_test.start("Construction")
+polypy_test.start("Construction and order")
 
 a1 = polypy.AlgebraicNumber((x**2 - 2)*(x - 10), 1)
 a2 = polypy.AlgebraicNumber((x**2 - 2), 1)
-print a1 < a2
-print a1
-print a2
+
+result = (a1 < a2)
+check_comparison(a1, a2, "<", result, False)
+
+result = (a1 == a2)
+check_comparison(a1, a2, "==", result, True)
 
 p = x**2
 zero = polypy.AlgebraicNumber(p, 0);
-print zero
+result = zero.to_double() == 0.0
+check_comparison(zero, 0, ".to_double ==", result, True)
 
 p = x**2 - 1
 one_neg = polypy.AlgebraicNumber(p, 0);
 one_pos = polypy.AlgebraicNumber(p, 1);
-print one_neg, one_pos
+result = (one_neg < one_pos)
+check_comparison(one_neg, one_pos, "<", result, True)
 
 p = x**2 - 2
 sqrt2_neg = polypy.AlgebraicNumber(p, 0);
 sqrt2_pos = polypy.AlgebraicNumber(p, 1);
-print sqrt2_neg, sqrt2_pos
+result = (sqrt2_neg < sqrt2_pos)
+check_comparison(sqrt2_neg, sqrt2_pos, "<", result, True)
 
 p = x**2 - 3
 sqrt3_neg = polypy.AlgebraicNumber(p, 0);
 sqrt3_pos = polypy.AlgebraicNumber(p, 1);
-print sqrt3_neg, sqrt3_pos 
+result = (sqrt3_neg < sqrt3_pos)
+check_comparison(sqrt3_neg, sqrt3_pos, "<", result, True)
 
-a = [zero, one_neg, sqrt2_pos, one_pos, sqrt2_neg]
-print a;
+a = [sqrt3_pos, zero, sqrt3_neg, one_neg, sqrt2_pos, one_pos, sqrt2_neg]
+a_sorted = [sqrt3_neg, sqrt2_neg, one_neg, zero, one_pos, sqrt2_pos, sqrt3_pos]
 a.sort()
-print a;
+
+result = (a == a_sorted)
+check_comparison(a, a_sorted, "==", result, True)
 
 a = []
+a_sorted = []
 for n in range(1, 10):
     p = x**2 - n
-    a.append(polypy.AlgebraicNumber(p, 0))
-    a.append(polypy.AlgebraicNumber(p, 1))
+    neg = polypy.AlgebraicNumber(p, 0)
+    pos = polypy.AlgebraicNumber(p, 1)
+    a.append(pos)
+    a.append(neg)
+    a_sorted.append(pos)
+    a_sorted.insert(0, neg)
 a.sort()
-print a
-a_double = [b.to_double() for b in a]
-print a_double
+check_comparison(a, a_sorted, "==", result, True)
 
 p1 = (x**2 - 2)*x        # -sqrt(2), 0, sqrt(2)
 p2 = (x**2 - 2)*(x-1)    # -sqrt(2), 1, sqrt(2)
@@ -56,13 +79,9 @@ a1 = polypy.AlgebraicNumber(p1, 2)
 a2 = polypy.AlgebraicNumber(p2, 2)
 a3 = polypy.AlgebraicNumber(p3, 2)
 
-a = [a1, a2, a3]
-print a
-a.sort()
-print a
-
-a_double = [x.to_double() for x in a]
-print a_double
+check_comparison(a1, a2, "==", (a1 == a2), True)
+check_comparison(a1, a3, "==", (a1 == a3), True)
+check_comparison(a2, a3, "==", (a2 == a3), True)
 
 polypy_test.start("Arithmetic")
 
@@ -73,23 +92,23 @@ polypy_test.start("Arithmetic")
 x = polypy.x
 
 a = polypy.AlgebraicNumber(x**2 - 2, 1)
-b = polypy.AlgebraicNumber(x**2 - 3, 1)
+a2 = polypy.AlgebraicNumber(x**2 - 4, 1)
 
 mul = a * a
-print mul, mul.to_double()
+check_comparison(mul, a2, "==", (mul == a2), True)
 
+b = polypy.AlgebraicNumber(x**2 - 3, 1)
+ab = polypy.AlgebraicNumber(x**2 - 6, 1)
 mul = a * b
-print mul
+check_comparison(mul, ab, "==", (mul == ab), True)
 
 a = polypy.AlgebraicNumber(x**2 - 2, 0)
 b = polypy.AlgebraicNumber(x**2 - 2, 1)
 add = a + b
-print add, add.to_double()
+check_comparison(add, zero, "==", (add == zero), True)
 
 a = polypy.AlgebraicNumber(x**2 - 2, 1)
 b = polypy.AlgebraicNumber(x**2 - 3, 1)
+ab = polypy.AlgebraicNumber(x**4 + -10*x**2 + 1, 3)
 add = a + b
-print add, add.to_double()
-
-
-
+check_comparison(add, ab, "==", (add == ab), True)
--- test/python/upolynomial_roots.py
+++ test/python/upolynomial_roots.py
@@ -5,7 +5,18 @@ import polypy_test
 
 polypy_test.init()
 
-polypy_test.start("Root finding")
+def check_roots_isolate(p, expected):
+    roots = p.roots_isolate()
+    sorted = all(roots[i] < roots[i+1] for i in xrange(len(roots)-1))
+    count = len(roots)
+    if ((not sorted) or count != expected):
+        print "p = ", p
+        print "sorted = ", sorted
+        print "count = ", count
+        print "expected = ", expected
+        polypy_test.check(False)
+    else:
+        polypy_test.check(True)
 
 def check_roots_count(p, expected, lb = None, ub = None):
     if (lb is None):
@@ -29,29 +40,24 @@ def check_roots_count(p, expected, lb = None, ub = None):
 
 x = polypy.x 
 
-p = 1*x**5 + (-23*x**4) + (-57*x**3) + 85*x**2 + 64*x + (-63)
-print p
-roots = p.roots_isolate()
-print roots
-
-p = 41*x**5 + (-79*x**4) + 44*x**3 + 56*x**2 + (-10*x) + 41
-print p
-roots = p.roots_isolate()
+polypy_test.start("Root isolation")
 
 p = x**2 - 1
-print p
-roots = p.roots_isolate()
-print roots
+check_roots_isolate(p, 2)
 
 p = (x - 1)*(x + 1)*(x - 2)
-print p
-roots = p.roots_isolate()
-print roots
+check_roots_isolate(p, 3)
+
+p = 1*x**5 + (-23*x**4) + (-57*x**3) + 85*x**2 + 64*x + (-63)
+check_roots_isolate(p, 5)
+
+p = 41*x**5 + (-79*x**4) + 44*x**3 + 56*x**2 + (-10*x) + 41
+check_roots_isolate(p, 1)
 
 p = 9*x**13 - 18*x**11 - 33*x**10 + 102*x**8 + 7*x**7 - 36*x**6 - 122*x**5 + 49*x**4 + 93*x**3 - 42*x**2 - 18*x** + 9
-print p
-roots = p.roots_isolate()
-print roots
+check_roots_isolate(p, 6)
+
+polypy_test.start("Root counting")
 
 p = x + 1 - x
 check_roots_count(p, 0)    
