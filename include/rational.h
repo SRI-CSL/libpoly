0b16d5231cc6417d2849972013cc73d1b7a485f4
Dejan Jovanovic <dejan.jovanovic@gmail.com>
2015-02-04 14:35:13 -0800

    removing ops from rational

--- include/rational.h
+++ include/rational.h
@@ -18,136 +18,129 @@
 /** Use the GMP rationals */
 typedef __mpq_struct lp_rational_t;
 
-/** Interface to the rationals */
-typedef struct {
+/**
+ * Construct rational 0/1.
+ */
+void lp_rational_construct(lp_rational_t* q);
 
-  /**
-   * Construct rational 0/1.
-   */
-  void (*construct) (lp_rational_t* q);
+/**
+ * Construct rational a/b.
+ */
+void lp_rational_construct_from_int(lp_rational_t* q, long a, unsigned long b);
 
-  /**
-   * Construct rational a/b.
-   */
-  void (*construct_from_int) (lp_rational_t* q, long a, unsigned long b);
+/**
+ * Construct rational a/1.
+ */
+void lp_rational_construct_from_integer(lp_rational_t* q, const lp_integer_t* a);
 
-  /**
-   * Construct rational a/1.
-   */
-  void (*construct_from_integer) (lp_rational_t* q, const lp_integer_t* a);
+/**
+ * Construct a rational representation of x.
+ */
+void lp_rational_construct_from_double(lp_rational_t* q, double x);
 
-  /**
-   * Construct a rational representation of x.
-   */
-  void (*construct_from_double) (lp_rational_t* q, double x);
+/**
+ * Construct a rational number from a dyadic rational.
+ */
+void lp_rational_construct_from_dyadic(lp_rational_t* q, const lp_dyadic_rational_t* qd);
 
-  /**
-   * Construct a rational number from a dyadic rational.
-   */
-  void (*construct_from_dyadic) (lp_rational_t* q, const lp_dyadic_rational_t* qd);
+/**
+ * Construct a copy of the given coefficient. The coefficient will be
+ * normalized according to the given ring.
+ */
+void lp_rational_construct_copy(lp_rational_t* q, const lp_rational_t* from);
 
-  /**
-   * Construct a copy of the given coefficient. The coefficient will be
-   * normalized according to the given ring.
-   */
-  void (*construct_copy) (lp_rational_t* q, const lp_rational_t* from);
+/**
+ * Assign the rational a given rational.
+ */
+void lp_rational_assign(lp_rational_t* q, const lp_rational_t* from);
 
-  /**
-   * Assign the rational a given rational.
-   */
-  void (*assign) (lp_rational_t* q, const lp_rational_t* from);
+/**
+ * Assign the rational a given a/b.
+ */
+void lp_rational_assign_int(lp_rational_t* q, long a, unsigned long b);
 
-  /**
-   * Assign the rational a given a/b.
-   */
-  void (*assign_int) (lp_rational_t* q, long a, unsigned long b);
+/**
+ * Deallocates the rational.
+ */
+void lp_rational_destruct(lp_rational_t* q);
 
-  /**
-   * Deallocates the rational.
-   */
-  void (*destruct) (lp_rational_t* q);
+/**
+ * Prints the rational to the given stream.
+ */
+int lp_rational_print(const lp_rational_t* c, FILE* out);
 
-  /**
-   * Prints the rational to the given stream.
-   */
-  int (*print) (const lp_rational_t* c, FILE* out);
+/**
+ * Returns the string representation of the rational.
+ */
+char* lp_rational_to_string(const lp_rational_t* q);
 
-  /**
-   * Returns the string representation of the rational.
-   */
-  char* (*to_string) (const lp_rational_t* q);
+/**
+ * Return the double representation of the rational.
+ */
+double lp_rational_to_double(const lp_rational_t* q);
 
-  /**
-   * Return the double representation of the rational.
-   */
-  double (*to_double) (const lp_rational_t* q);
+/**
+ * Returns the sign of the rational.
+ */
+int lp_rational_sgn(const lp_rational_t* q);
 
-  /**
-   * Returns the sign of the rational.
-   */
-  int (*sgn) (const lp_rational_t* q);
+/**
+ * Compare the two rationals.
+ */
+int lp_rational_cmp(const lp_rational_t* q1, const lp_rational_t* q2);
 
-  /**
-   * Compare the two rationals.
-   */
-  int (*cmp) (const lp_rational_t* q1, const lp_rational_t* q2);
+/**
+ * Swap two rationals.
+ */
+void lp_rational_swap(lp_rational_t* q1, lp_rational_t* q2);
 
-  /**
-   * Swap two rationals.
-   */
-  void (*swap) (lp_rational_t* q1, lp_rational_t* q2);
+/**
+ * Compute sum = a + b.
+ */
+void lp_rational_add(lp_rational_t* sum, const lp_rational_t* a, const lp_rational_t* b);
 
-  /**
-   * Compute sum = a + b.
-   */
-  void (*add) (lp_rational_t* sum, const lp_rational_t* a, const lp_rational_t* b);
+/**
+ * Compute sum = a + b.
+ */
+void lp_rational_add_integer(lp_rational_t* sum, const lp_rational_t* a, const lp_integer_t* b);
 
-  /**
-   * Compute sum = a + b.
-   */
-  void (*add_integer) (lp_rational_t* sum, const lp_rational_t* a, const lp_integer_t* b);
+/**
+ * Compute sub = a - b.
+ */
+void lp_rational_sub(lp_rational_t* sub, const lp_rational_t* a, const lp_rational_t* b);
 
-  /**
-   * Compute sub = a - b.
-   */
-  void (*sub) (lp_rational_t* sub, const lp_rational_t* a, const lp_rational_t* b);
+/**
+ * Compute neg = -a.
+ */
+void lp_rational_neg(lp_rational_t* neg, const lp_rational_t* a);
 
-  /**
-   * Compute neg = -a.
-   */
-  void (*neg) (lp_rational_t* neg, const lp_rational_t* a);
+/**
+ * Compute the inverse of a. Assumes a != 0.
+ */
+void lp_rational_inv(lp_rational_t* inv, const lp_rational_t* a);
 
-  /**
-   * Compute the inverse of a. Assumes a != 0.
-   */
-  void (*inv) (lp_rational_t* inv, const lp_rational_t* a);
+/**
+ * Compute product = a * b.
+ */
+void lp_rational_mul(lp_rational_t* mul, const lp_rational_t* a, const lp_rational_t* b);
 
-  /**
-   * Compute product = a * b.
-   */
-  void (*mul) (lp_rational_t* mul, const lp_rational_t* a, const lp_rational_t* b);
+/**
+ * Compute product = a*2^n
+ */
+void lp_rational_mul_2exp(lp_rational_t* mul, const lp_rational_t* a, unsigned n);
 
-  /**
-   * Compute product = a*2^n
-   */
-  void (*mul_2exp) (lp_rational_t* mul, const lp_rational_t* a, unsigned n);
+/**
+ * Compute power = a^n in the given ring.
+ */
+void lp_rational_pow(lp_rational_t* pow, const lp_rational_t* a, unsigned n);
 
-  /**
-   * Compute power = a^n in the given ring.
-   */
-  void (*pow) (lp_rational_t* pow, const lp_rational_t*a, unsigned n);
+/**
+ * Compute a/b = div.
+ */
+void lp_rational_div(lp_rational_t* div, const lp_rational_t* a, const lp_rational_t* b);
 
-  /**
-   * Compute a/b = div.
-   */
-  void (*div) (lp_rational_t* div, const lp_rational_t* a, const lp_rational_t* b);
+/**
+ * Compute div = a/2^n
+ */
+void lp_rational_div_2exp(lp_rational_t* div, const lp_rational_t* a, unsigned n);
 
-  /**
-   * Compute div = a/2^n
-   */
-  void (*div_2exp) (lp_rational_t* div, const lp_rational_t* a, unsigned n);
-
-} lp_rational_ops_t;
-
-/** Implementation */
-extern const lp_rational_ops_t lp_rational_ops;
--- src/number/rational.c
+++ src/number/rational.c
@@ -7,30 +7,102 @@
 
 #include "number/rational.h"
 
-const lp_rational_ops_t lp_rational_ops = {
-    rational_construct,
-    rational_construct_from_int,
-    rational_construct_from_integer,
-    rational_construct_from_double,
-    rational_construct_from_dyadic,
-    rational_construct_copy,
-    rational_assign,
-    rational_assign_int,
-    rational_destruct,
-    rational_print,
-    rational_to_string,
-    rational_to_double,
-    rational_sgn,
-    rational_cmp,
-    rational_swap,
-    rational_add,
-    rational_add_integer,
-    rational_sub,
-    rational_neg,
-    rational_inv,
-    rational_mul,
-    rational_mul_2exp,
-    rational_pow,
-    rational_div,
-    rational_div_2exp
-};
+void lp_rational_construct(lp_rational_t* q) {
+  rational_construct(q);
+}
+
+void lp_rational_construct_from_int(lp_rational_t* q, long a, unsigned long b) {
+  rational_construct_from_int(q, a, b);
+}
+
+void lp_rational_construct_from_integer(lp_rational_t* q, const lp_integer_t* a) {
+  rational_construct_from_integer(q, a);
+}
+
+void lp_rational_construct_from_double(lp_rational_t* q, double x) {
+  rational_construct_from_double(q, x);
+}
+
+void lp_rational_construct_from_dyadic(lp_rational_t* q, const lp_dyadic_rational_t* qd) {
+  rational_construct_from_dyadic(q, qd);
+}
+
+void lp_rational_construct_copy(lp_rational_t* q, const lp_rational_t* from) {
+  rational_construct_copy(q, from);
+}
+
+void lp_rational_assign(lp_rational_t* q, const lp_rational_t* from) {
+  rational_assign(q, from);
+}
+
+void lp_rational_assign_int(lp_rational_t* q, long a, unsigned long b) {
+  rational_assign_int(q, a, b);
+}
+
+void lp_rational_destruct(lp_rational_t* q) {
+  rational_destruct(q);
+}
+
+int lp_rational_print(const lp_rational_t* c, FILE* out) {
+  return rational_print(c, out);
+}
+
+char* lp_rational_to_string(const lp_rational_t* q) {
+  return rational_to_string(q);
+}
+
+double lp_rational_to_double(const lp_rational_t* q) {
+  return rational_to_double(q);
+}
+
+int lp_rational_sgn(const lp_rational_t* q) {
+  return rational_sgn(q);
+}
+
+int lp_rational_cmp(const lp_rational_t* q1, const lp_rational_t* q2) {
+  return rational_cmp(q1, q2);
+}
+
+void lp_rational_swap(lp_rational_t* q1, lp_rational_t* q2) {
+  rational_swap(q1, q2);
+}
+
+void lp_rational_add(lp_rational_t* sum, const lp_rational_t* a, const lp_rational_t* b) {
+  rational_add(sum, a, b);
+}
+
+void lp_rational_add_integer(lp_rational_t* sum, const lp_rational_t* a, const lp_integer_t* b) {
+  rational_add_integer(sum, a, b);
+}
+
+void lp_rational_sub(lp_rational_t* sub, const lp_rational_t* a, const lp_rational_t* b) {
+  rational_sub(sub, a, b);
+}
+
+void lp_rational_neg(lp_rational_t* neg, const lp_rational_t* a) {
+  rational_neg(neg, a);
+}
+
+void lp_rational_inv(lp_rational_t* inv, const lp_rational_t* a) {
+  rational_inv(inv, a);
+}
+
+void lp_rational_mul(lp_rational_t* mul, const lp_rational_t* a, const lp_rational_t* b) {
+  rational_mul(mul, a, b);
+}
+
+void lp_rational_mul_2exp(lp_rational_t* mul, const lp_rational_t* a, unsigned n) {
+  rational_mul_2exp(mul, a, n);
+}
+
+void lp_rational_pow(lp_rational_t* pow, const lp_rational_t*a, unsigned n) {
+  rational_pow(pow, a, n);
+}
+
+void lp_rational_div(lp_rational_t* div, const lp_rational_t* a, const lp_rational_t* b) {
+  rational_div(div, a, b);
+}
+
+void lp_rational_div_2exp(lp_rational_t* div, const lp_rational_t* a, unsigned n) {
+  rational_div_2exp(div, a, n);
+}
