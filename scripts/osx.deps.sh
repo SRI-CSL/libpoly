58b4a18d2994451c8e91274592458c9acc5914c3
Bruno Dutertre <bruno@csl.sri.com>
2018-04-25 21:42:07 -0700

    Cleanup fix for travis

--- scripts/osx.deps.sh
+++ scripts/osx.deps.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 brew outdated cmake || brew upgrade cmake
 brew outdated gmp || brew upgrade gmp
-pip2 install sympy
+
+#
+# HACK
+#
+# Some versions of brew instal python2.7 as python2
+# but do not create /usr/local/bin/python.
+#
+# Because there doesn't seem to be way to force cmake to
+# use the python we want (i.e., not /usr/bin/python), we
+# create the missing symbolic link here.
+#
+# This all assumes that /usr/local/bin is before /usr/bin
+# on the PATH, which seems to be true for travis.
+#
+for p in python python-config ;
+do
+    q=`echo $p | sed -e 's/python/python2/' `
+    default=`which $p`
+    variant=`which $q`
+    if [ "$default" == "/usr/bin/$p" ] && [ "x$variant" != "x" ] ; then
+       ln -s $variant /usr/local/bin/$p
+    fi
+done
+
+#
+# Make sure we also have pip
+#
+default=`which pip`
+variant=`which pip2`
+if [ "x$default" == "x" ] && [ "x$variant" != "x" ] ; then
+    ln -s $variant /usr/local/bin/pip
+fi
+
+pip install sympy
