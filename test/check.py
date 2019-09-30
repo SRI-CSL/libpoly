e31c4db2d5f956714322c8ba62ca584375b8b3bd
Ian A. Mason <iam@csl.sri.com>
2019-09-30 11:40:16 -0700

    Gave check.py a main

--- test/check.py
+++ test/check.py
@@ -4,34 +4,6 @@ import polypy
 import polypy_test
 
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--sympy', action="store_true")
-parser.add_argument('--stats', action="store_true")
-
-args = parser.parse_args()
-
-tests = ["python/upolynomial_gcd.py",
-         "python/upolynomial_factor.py",
-         "python/upolynomial_roots.py",
-         "python/algebraic_number.py",
-         "python/variable.py",
-         "python/polynomial_arithmetic.py",
-         "python/polynomial_sgn.py",
-         "python/polynomial_gcd.py",
-         "python/polynomial_factorization.py",
-         "python/polynomial_eval.py",
-         "python/polynomial_roots.py",
-         "python/polynomial_resultants.py",
-         "python/polynomial_feasibility.py"]
-
-if (args.sympy):
-    print("Sympy checking enabled")
-    polypy_test.sympy_checker.enabled = True
-else:
-    print("Sympy checking disabled")
-    polypy_test.sympy_checker.enabled = False
-
-
 def forkexec(test, env):
     if sys.version_info >= (3,0):  #IAM: (3, 2) might be more accurate...
         with open(test) as testf:
@@ -40,14 +12,50 @@ def forkexec(test, env):
     else:
         execfile(test, env, env)
 
-for test in tests:
-    print("Running {0}:".format(test))
-    context = dict()
-    forkexec(test, context)
-    module = context["polypy_test"]
-    print("PASS: {0}".format(module.PASS))
-    print("FAIL: {0}".format(module.FAIL))
 
-if (args.stats):
-    print("Statistics:")
-    polypy.stats_print()
+def main():
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--sympy', action="store_true")
+    parser.add_argument('--stats', action="store_true")
+
+    args = parser.parse_args()
+
+    tests = ["python/upolynomial_gcd.py",
+             "python/upolynomial_factor.py",
+             "python/upolynomial_roots.py",
+             "python/algebraic_number.py",
+             "python/variable.py",
+             "python/polynomial_arithmetic.py",
+             "python/polynomial_sgn.py",
+             "python/polynomial_gcd.py",
+             "python/polynomial_factorization.py",
+             "python/polynomial_eval.py",
+             "python/polynomial_roots.py",
+             "python/polynomial_resultants.py",
+             "python/polynomial_feasibility.py"]
+
+    if (args.sympy):
+        print("Sympy checking enabled")
+        polypy_test.sympy_checker.enabled = True
+    else:
+        print("Sympy checking disabled")
+        polypy_test.sympy_checker.enabled = False
+
+
+
+    for test in tests:
+        print("Running {0}:".format(test))
+        context = dict()
+        forkexec(test, context)
+        module = context["polypy_test"]
+        print("PASS: {0}".format(module.PASS))
+        print("FAIL: {0}".format(module.FAIL))
+
+    if (args.stats):
+        print("Statistics:")
+        polypy.stats_print()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
