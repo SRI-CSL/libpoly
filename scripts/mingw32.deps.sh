eaa0569ca29859be089e1e7ae5f077e19bcfb70d
Dejan Jovanovic <dejan.jovanovic@sri.com>
2020-09-21 17:23:31 -0400

    fiddling with cross compile

---
+++ scripts/mingw32.deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pushd .
+wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.xz
+tar xvf gmp-6.2.0.tar.xz
+cd gmp-6.2.0
+./configure --host=x86_64-w64-mingw32 --enable-cxx --prefix=$HOME/mingw32
+make
+make install
+popd
+
+
