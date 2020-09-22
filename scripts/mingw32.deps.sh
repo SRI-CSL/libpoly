95d46cf0023342c46f5e2a37e54ebda6d86f3748
Dejan Jovanovic <dejan.jovanovic@sri.com>
2020-09-22 12:03:35 -0400

    Revert "..."
    This reverts commit 0f145dec535dd20838347bb80c6bc7c3364b742f.

--- scripts/mingw32.deps.sh
+++ scripts/mingw32.deps.sh
@@ -4,7 +4,7 @@ pushd .
 wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.xz
 tar xvf gmp-6.2.0.tar.xz
 cd gmp-6.2.0
-./configure --host=x86_64-w64-mingw32 --enable-cxx --prefix=$HOME/prefix
+./configure --host=x86_64-w64-mingw32 --build=$(gcc -dumpmachine) --enable-cxx --prefix=$HOME/prefix
 make
 make install
 popd
