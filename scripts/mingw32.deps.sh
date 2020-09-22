e85819373421f232a5d2a29e4fa28ff860879bce
Dejan Jovanovic <dejan.jovanovic@sri.com>
2020-09-22 10:59:32 -0400

    ...

--- scripts/mingw32.deps.sh
+++ scripts/mingw32.deps.sh
@@ -4,7 +4,7 @@ pushd .
 wget https://gmplib.org/download/gmp/gmp-6.2.0.tar.xz
 tar xvf gmp-6.2.0.tar.xz
 cd gmp-6.2.0
-./configure --host=x86_64-w64-mingw32 --build=--build=$(gcc -dumpmachine) --enable-cxx
+./configure --host=x86_64-w64-mingw32 --build=$(gcc -dumpmachine) --enable-cxx
 make
 make install
 popd
