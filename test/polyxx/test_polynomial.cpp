5f081b599dc2332f250429e42f4ca93449f7d021
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-10-16 15:30:49 +0200

    Reduce coefficients in test to make all fit into 32bit. Fixes #47.

--- test/polyxx/test_polynomial.cpp
+++ test/polyxx/test_polynomial.cpp
@@ -44,31 +44,31 @@ TEST_CASE("polynomial::properties") {
 TEST_CASE("polynomial::resultant") {
   Variable y("y");
   Variable x("x");
-  Polynomial p = 7*pow(x, 6) + 14*pow(x, 5) + 7*y - 1;
-  Polynomial q = 42*pow(x, 5) + 70*pow(x, 4);
+  Polynomial p = 1 * pow(x, 6) + 2 * pow(x, 5) + 3 * y - 1;
+  Polynomial q = 7 * pow(x, 5) + 5 * pow(x, 4);
   Polynomial r = resultant(p, q);
-  CHECK(r == 92254156521408*pow(y,5) - 461361174686720*pow(y,4) + 244807578081920*pow(y,3) - 51113953954560*pow(y,2) + 4803956911040*y - 170197631744);
+  CHECK(r == 28588707 * pow(y, 5) - 49925970 * pow(y, 4) + 34802730 * pow(y, 3) - 12107160 * pow(y, 2) + 2102235 * y - 145774);
 }
 
 TEST_CASE("polynomial::discriminant") {
   Variable y("y");
   Variable x("x");
-  Polynomial p = 7*pow(x, 6) + 14*pow(x, 5) + 7*y - 1;
+  Polynomial p = 1 * pow(x, 6) + 2 * pow(x, 5) + 3 * y - 1;
   Polynomial d = discriminant(p);
-  CHECK(d == 13179165217344*pow(y,5) - 65908739240960*pow(y,4) + 34972511154560*pow(y,3) - 7301993422080*pow(y,2) + 686279558720*y - 24313947392);
+  CHECK(d == 11337408 * pow(y, 5) - 35095680 * pow(y, 4) + 34197120 * pow(y, 3) - 14999040 * pow(y, 2) + 3099840 * y - 246656);
 }
 TEST_CASE("polynomial::psc") {
   Variable y("y");
   Variable x("x");
-  Polynomial p = 7*pow(x, 6) + 14*pow(x, 5) + 7*y - 1;
-  Polynomial q = 42*pow(x, 5) + 70*pow(x, 4);
+  Polynomial p = 1 * pow(x, 6) + 2 * pow(x, 5) + 3 * y - 1;
+  Polynomial q = 7 * pow(x, 5) + 5 * pow(x, 4);
   auto tmp = psc(p, q);
 
   CHECK(tmp.size() == 6);
-  CHECK(tmp[0] == 92254156521408*pow(y,5) - 461361174686720*pow(y,4) + 244807578081920*pow(y,3) - 51113953954560*pow(y,2) + 4803956911040*y - 170197631744);
-  CHECK(tmp[1] == 174327582240*y*y*y-74711820960*y*y+10673117280*y-508243680);
+  CHECK(tmp[0] == 28588707 * pow(y, 5) - 49925970 * pow(y, 4) + 34802730 * pow(y, 3) - 12107160 * pow(y, 2) + 2102235 * y - 145774);
+  CHECK(tmp[1] == 416745 * y * y * y - 416745 * y * y + 138915 * y - 15435);
   CHECK(tmp[2] == Integer(0));
   CHECK(tmp[3] == Integer(0));
-  CHECK(tmp[4] == Integer(-6860));
-  CHECK(tmp[5] == Integer(42));
+  CHECK(tmp[4] == Integer(-45));
+  CHECK(tmp[5] == Integer(7));
 }
