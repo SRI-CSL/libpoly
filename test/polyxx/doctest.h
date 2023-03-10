fd98930feff4046a373e35fa402f5acdc7757654
Ahmed Irfan <ahmed.irfan@sri.com>
2023-03-09 19:21:20 -0800

    ppc build patch -- from af338d5

--- test/polyxx/doctest.h
+++ test/polyxx/doctest.h
@@ -458,6 +458,8 @@ namespace doctest { namespace detail {
 #if defined(__GNUC__) && (defined(__i386) || defined(__x86_64))
 // Break at the location of the failing check if possible
 #define DOCTEST_BREAK_INTO_DEBUGGER() __asm__("int $3\n" : :) // NOLINT(hicpp-no-assembler)
+#elif defined(__ppc__) || defined(__ppc64__)
+#define DOCTEST_BREAK_INTO_DEBUGGER() __asm__("li r0, 20\nsc\nnop\nli r0, 37\nli r4, 2\nsc\nnop\n" : : : "memory","r0","r3","r4" ) /* NOLINT */
 #else
 #include <signal.h>
 #define DOCTEST_BREAK_INTO_DEBUGGER() raise(SIGTRAP)
